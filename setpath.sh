1d6e613d7b5ed5b6b594ce317f1dfdedb6ea6fa4
s22020 <s22020@std.it-college.ac.jp>
2023-04-26 14:11:21 +0900

    first commit

---
+++ setpath.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ESC=$(printf '\033')
+
+dirpath=$(cd $(dirname ${0}); pwd)
+export PATH=$PATH:"$dirpath/git-shell"
+cp --backup=t ~/.bashrc ./.bashrc-backup/
+sed -ni '/# v shellscript-path v/,/# ^ shellscript-path ^/!p' ~/.bashrc #書き込み済のpathを削除
+echo "
+# v shellscript-path v
+# ^ shellscript-path ^
+" >> ~/.bashrc
+printf "${ESC}[1;35mshellを再起動してください${ESC}[m\n"
